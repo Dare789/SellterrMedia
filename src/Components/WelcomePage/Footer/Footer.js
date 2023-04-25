37a2cf0b4096bcfa8a8d4bb6a96117420572248e
Emmanuel <emmanuel@macs-MacBook-Pro.local>
2023-04-25 12:58:16 +0100

    fixed footer style

--- src/Components/WelcomePage/Footer/Footer.js
+++ src/Components/WelcomePage/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
 				<img src={Sellterr} alt="Sellterr" className="cursor-pointer h-10 w-10 lg:h-14 lg:w-14 shadow-transparent"/>
         <div className='text-xl lg:text-2xl text-black font-extrabold'>Sellterr Media</div>
 		</div>
-    <div className="flex mx-5 sm:mx-5 items-center justify-center space-x-2 pt-3">
+    <div className="flex mx-5 sm:mx-5 items-center justify-center space-x-2 pt-1">
       <div className="flex space-x-5">
                   <div className="font-bold text-md text-center text-sm lg:text-lg cursor-pointer">About</div>
 				          <div className="font-bold text-md text-center text-sm lg:text-lg cursor-pointer">Terms Of Use</div>
@@ -22,7 +22,7 @@ const Footer = () => {
       </h1> */}
       {/* <h1 className='flex mx-5 sm:mx-5 items-center justify-center space-x-2'>CA, 90503, USA</h1> */}
     </div>
-    <div className='flex space-x-4 mx-5 sm:mx-5 items-center justify-center py-3'>
+    <div className='flex space-x-4 mx-5 sm:mx-5 items-center justify-center py-2'>
       <SocialIcon network="twitter" bgColor="black" style={{ height: 32, width: 32 }} className="cursor-pointer"/>
       <SocialIcon network="facebook" bgColor="black" style={{ height: 32, width: 32 }} className="cursor-pointer"/>
       <SocialIcon network="instagram" bgColor="black" style={{ height: 32, width: 32 }} className="cursor-pointer"/>
